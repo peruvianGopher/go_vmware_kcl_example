c5bb9c9ede585d8a3dbcbbc1db58fbc9df9fbf1b
Massimo Ludena <massimo.ludena.work@gmail.com>
2021-11-09 20:51:40 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gokcltest
+
+go 1.16
+
+require github.com/vmware/vmware-go-kcl v1.5.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vmware/vmware-go-kcl/logger"
+)
+
+func main() {
+	fmt.Println(logger.Debug)
+}
