258b3bf23615b45845a2c7279ad5fd081a4e6f4b
Massimo Ludeña <massimo.ludena@LIM-LP1779.local>
2021-11-10 20:45:03 -0500

    implementing kcl interface

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gokcltest
 
 go 1.16
 
-require github.com/vmware/vmware-go-kcl v1.5.0
+require (
+	github.com/aws/aws-sdk-go v1.41.7
+	github.com/vmware/vmware-go-kcl v1.5.0
+)
--- main.go
+++ main.go
@@ -2,9 +2,35 @@ package main
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/vmware/vmware-go-kcl/clientlibrary/interfaces"
 	"github.com/vmware/vmware-go-kcl/logger"
 )
 
+const (
+	STREAM_NAME = "abc"
+	REGION = "us-west-2"
+)
+
 func main() {
+
+
 	fmt.Println(logger.Debug)
+}
+
+type consumer struct {
+
+}
+
+func (c consumer) Initialize(initializationInput *interfaces.InitializationInput) {
+	initializationInput.ShardId = "a"
+	panic("implement me")
+}
+
+func (c consumer) ProcessRecords(processRecordsInput *interfaces.ProcessRecordsInput) {
+	panic("implement me")
+}
+
+func (c consumer) Shutdown(shutdownInput *interfaces.ShutdownInput) {
+	panic("implement me")
 }
